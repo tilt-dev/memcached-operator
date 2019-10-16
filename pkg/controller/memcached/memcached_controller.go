25450deef7bdb13c704c38377244c55b24bd4b68
Dan Miller <dmiller@windmill.engineering>
2019-10-16 11:19:11 -0400

    fix build

--- pkg/controller/memcached/memcached_controller.go
+++ pkg/controller/memcached/memcached_controller.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"reflect"
 
-	cachev1alpha1 "github.com/example-inc/memcached-operator/pkg/apis/cache/v1alpha1"
+	cachev1alpha1 "github.com/windmilleng/memcached-operator/pkg/apis/cache/v1alpha1"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
